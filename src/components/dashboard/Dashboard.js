4675a59fd5d15960be66504a3810a9b1133a8c61
Milexys Ferrer <milexys.ferrerp@gmail.com>
2019-04-10 16:30:35 -0400

    codigo reducido y scroll funcionando

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -13,10 +13,6 @@ class Dashboard extends Component {
         super()
         this.state = {
             images: [],
-            images0: [],
-            images1: [],
-            images2: [],
-            images3: [],
             start: 1,
             count: 20,
         }
@@ -26,132 +22,63 @@ class Dashboard extends Component {
        const { start, count } = this.state
         unsplash.photos.listPhotos(start, count, "latest")
         .then(response => response.json())
-        .then(data => this.setState({
-            ...this.state,
-            images: data
-        }))
-        .then(data => {
-            let img0 = [];
-            let img1 = [];
-            let img2 = [];
-            let img3 = [];
-
-            for (let i = 0; i < 5; i++){
-                let randomImg = this.state.images[i]
-                img0.push(randomImg.urls.full);
-            }
-            for (let i = 5; i < 10; i++){
-                let randomImg = this.state.images[i]
-                img1.push(randomImg.urls.full);
-            }
-            for (let i = 10; i < 15; i++){
-                let randomImg = this.state.images[i]
-                img2.push(randomImg.urls.full);
-            }
-            for (let i = 15; i < 20; i++){
-                let randomImg = this.state.images[i]
-                img3.push(randomImg.urls.full);
+        .then(toJson => {
+            let data = toJson;
+            let imgURL = []
+            for (let i = 0; i < 20; i ++){
+                let img = data[i]
+                imgURL.push(img.urls.thumb)
             }
             this.setState({
                 ...this.state,
-                    images0: img0,
-                    images1: img1,
-                    images2: img2,
-                    images3: img3,
-               })
+                images: imgURL
+            })
         })
     }
 
     handleMore = () => {
         const { start, count } = this.state
+        let start1 = start + count;
         this.setState({
-            start: this.state.start + count,
+            start: start1,
         })
-        unsplash.photos.listPhotos(start, count, "latest")
+        unsplash.photos.listPhotos(start1, count, "latest")
         .then(response => response.json())
-        .then(data => this.setState({
-            ...this.state,
-            images: this.state.images.concat(data)
-        }))
-        .then(data => {
-            let img0 = [];
-            let img1 = [];
-            let img2 = [];
-            let img3 = [];
-
-            for (let i = 20; i < 25; i++){
-                let randomImg = this.state.images[i]
-                img0.push(randomImg.urls.full);
-            }
-            for (let i = 25; i < 30; i++){
-                let randomImg = this.state.images[i]
-                img1.push(randomImg.urls.full);
-            }
-            for (let i = 30; i < 35; i++){
-                let randomImg = this.state.images[i]
-                img2.push(randomImg.urls.full);
-            }
-            for (let i = 35; i < 40; i++){
-                let randomImg = this.state.images[i]
-                img3.push(randomImg.urls.full);
+        .then(toJson => {
+            let newData = toJson
+            let imgNewURL = [];
+            for (let i = 0; i < 20; i++){
+                let img = newData[i]
+                imgNewURL.push(img.urls.thumb)
             }
             this.setState({
                 ...this.state,
-                    images0: this.state.images0.concat(img0),
-                    images1: this.state.images0.concat(img1),
-                    images2: this.state.images0.concat(img2),
-                    images3: this.state.images0.concat(img3),
-               })
+                images: this.state.images.concat(imgNewURL)
+            })
         })
+      console.log(this.state.images)
     }
-
+    openModal(){
+        
+    }
     render(){
-        const column0 = this.state.images0.map(image =>{
-            return (   
-                <div className="card" key={image.id}>
-                    <img className="image" src={image} alt="img"/>
-                    <i className="material-icons">more_horiz</i>
-                </div>
-            )
-        })
-        const column1 = this.state.images1.map(image =>{
-            return (   
-                <div className="card" key={image.id}>
-                    <img className="image" src={image} alt="img"/>
-                    <i className="material-icons">more_horiz</i>
-                </div>
-            )
-        })
-        const column2 = this.state.images2.map(image =>{
-            return (   
-                <div className="card" key={image.id}>
-                    <img className="image" src={image} alt="img"/>
-                    <i className="material-icons">more_horiz</i>
-                </div>
-            )
-        })
-        const column3 = this.state.images3.map(image =>{
-            return (   
-                <div className="card" key={image.id}>
-                    <img className="image" src={image} alt="img"/>
-                    <i className="material-icons">more_horiz</i>
-                </div>
-            )
-        })
         return (
-            <div className="container">
+            <div className="masonry">
                 <InfiniteScroll
                 dataLength={this.state.images.length}
                 next={this.handleMore}
                 hasMore={true}
-                loader={<h4>Loading...</h4>}
+                loader={<h4 style={{textAlign: "center"}}>Loading...</h4>}
                 >
-                    <div className="row">
-                        <div className="column">{column0}</div>
-                        <div className="column">{column1}</div>
-                        <div className="column">{column2}</div>
-                        <div className="column">{column3}</div>
-                    </div>
+                      {this.state.images.map(image => {
+                        return (
+                            <div className="item" key={image.id}>
+                                <img className="image" src={image} onClick={this.openModal} alt="img"/>
+                                <i className="material-icons">more_horiz</i>
+                           </div>
+                        )
+                    })
+                    }
                 </InfiniteScroll>
             </div>
         ) 
