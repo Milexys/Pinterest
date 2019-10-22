b547ce9bd96cad0292af9b2d503d013654bb5388
milexys <milexys.ferrerp@gmail.com>
2019-10-22 16:45:02 -0300

    agregando SASS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
-
+import './css/styles.css';
 class App extends Component {
   render() {
     return (
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,16 +5,18 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div className="imgLogo">
-            <i className='fab fa-pinterest'></i>
+                <i className='fab fa-pinterest'></i>
             </div>
             <div className="inputSearch">
-                <input type="text" placeholder="Buscar" />
-                <i className="material-icons">search</i>
+                <div className="input">
+                    <i className="material-icons">search</i>
+                    <input type="text" placeholder="Buscar" />
+                </div>
             </div>
             <div className="buttons">
-                <a href='/inicio' className="btn" style={{color:'black'}}>Inicio</a> 
-                <a href='/siguiendo' className="btn">Siguiendo</a> 
-                <a href='/perfil' className="btn">Milexys<i className='fas fa-chevron-down' style={{fontSize:'14px'}}></i></a>            
+                <a href='/inicio' className="btn" style={{ color: 'black' }}>Inicio</a>
+                <a href='/siguiendo' className="btn">Siguiendo</a>
+                <a href='/perfil' className="btn"style={{display:'flex'}}><div style={{margin: '0 0.5em'}}><p>M</p></div>Milexys</a>
                 <a href='/chat' className="btn" ><i className='fas fa-comment-dots'></i></a>
                 <a href='/notif' className="btn"><i className='fas fa-bell'></i></a>
                 <a href='/menu' className="btn"><i className="material-icons">more_horiz</i></a>
